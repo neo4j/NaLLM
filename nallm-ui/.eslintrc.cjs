df0c1ba8030532a210f49a775309b2289f6d5326
Jon Harris <harris.jb@gmail.com>
2023-05-09 23:20:24 -0400

    Add initial very basic ui using react 17 & ndl

---
+++ nallm-ui/.eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '17.0' } },
+  plugins: [],
+  rules: {},
+}
---
+++ nallm-ui/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from '@neo4j-ndl/react';
+
+import '@neo4j-ndl/base/lib/neo4j-ds-styles.css';
+
+function App() {
+  return (
+    <div>
+      Hello World
+      <Button>Click</Button>
+    </div>
+  );
+}
+
+export default App;
