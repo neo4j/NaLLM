df87ea662d467eb0f255565435328eebe9069f25
Noah Mayerhofer <noahmay@users.noreply.github.com>
2023-05-17 13:53:22 +0200

    update react to 18

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,13 +1,9 @@
-import { Button, TextLink } from "@neo4j-ndl/react";
-
 import "@neo4j-ndl/base/lib/neo4j-ds-styles.css";
 
 function App() {
   return (
     <div className="flex place-items-center min-w-[320px] min-h-[100vh]">
       Hello World
-      <Button>Click</Button>
-      <TextLink href="https://neo4j.com">Neo4j</TextLink>
     </div>
   );
 }
