0bcc232f6402c9c49c81415b9997745fa4fe7699
Noah Mayerhofer <noahmay@users.noreply.github.com>
2023-05-17 14:39:00 +0200

    setup of ui for multiple use cases

---
+++ ui/src/chat-with-kg/App.tsx
@@ -0,0 +1,11 @@
+import "@neo4j-ndl/base/lib/neo4j-ds-styles.css";
+
+function App() {
+  return (
+    <div className="flex place-items-center min-w-[320px] min-h-[100vh]">
+      This is the chat with knowledge graph app.
+    </div>
+  );
+}
+
+export default App;
