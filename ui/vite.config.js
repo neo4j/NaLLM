0b4de883e8be480f372ee8edeacc0105b864299b
Noah Mayerhofer <noahmay@users.noreply.github.com>
2023-05-22 09:23:45 +0200

    setup of ui for multiple use cases (#9)

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -1,7 +1,16 @@
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        appKgChat: fileURLToPath(new URL('./use-cases/chat-with-kg/index.html', import.meta.url)),
+        appImport: fileURLToPath(new URL('./use-cases/unstructured-import/index.html', import.meta.url)),
+      },
+    },
+  },
 })
