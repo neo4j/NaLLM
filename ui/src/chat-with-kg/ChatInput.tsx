2bc73d28fe38895441fdf8d462032e9acde95321
Noah Mayerhofer <noahmay@users.noreply.github.com>
2023-05-31 12:08:15 +0200

    fix attribute

--- ui/src/chat-with-kg/ChatInput.tsx
+++ ui/src/chat-with-kg/ChatInput.tsx
@@ -57,7 +57,7 @@ function ChatInput(props: ChatInputProps) {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
             aria-hidden="true"
             className="w-6 h-6 text-light-neutral-text-weak"
