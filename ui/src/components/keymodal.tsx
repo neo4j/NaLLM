675219997017e96e021c02992cc41a9086da12ba
Jon Harris <harris.jb@gmail.com>
2023-06-30 08:05:39 -0400

    improve api key modal styling

--- ui/src/components/keymodal.tsx
+++ ui/src/components/keymodal.tsx
@@ -29,13 +29,20 @@ function KeyModal(props: KeyModalProps) {
       onRequestClose={onRequestClose}
       style={customModalStyles}
       contentLabel="Enter Open API Key"
+      className="ndl-modal ndl-dialog ndl-with-close-button ndl-medium"
     >
-      <div className="flex justify-end">
-        <button onClick={onCloseModal}>close</button>
-      </div>
-      <div className="flex">
-        <h2>Please Enter Your OpenAI API Key</h2>
-        
+      <button role="button" aria-label="close" className="ndl-icon-btn ndl-dialog-close ndl-large ndl-clean" onClick={onCloseModal}>
+        <div className="ndl-icon">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+          </svg>
+        </div>
+      </button>
+      <div className="flex flex-nowrap">
+        <div className="flex flex-col flex-1 w-full">
+          <h4 className="ndl-dialog-header">API Key</h4>
+          <div className="ndl-dialog-subtitle body-large">Please enter your OpenAI API Key</div>
+        </div>
       </div>
       <form>
         <div className="ndl-form-item">
