9190bc15d17216274250d65a44436b2a3e892dc1
Jon Harris <harris.jb@gmail.com>
2023-06-30 08:06:18 -0400

    fix react propname

--- ui/src/components/keymodal.tsx
+++ ui/src/components/keymodal.tsx
@@ -33,7 +33,7 @@ function KeyModal(props: KeyModalProps) {
     >
       <button role="button" aria-label="close" className="ndl-icon-btn ndl-dialog-close ndl-large ndl-clean" onClick={onCloseModal}>
         <div className="ndl-icon">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
           </svg>
         </div>
