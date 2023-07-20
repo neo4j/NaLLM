6fdde099156ed51432eac5036a9426b06222c531
Vetri190591 <139077431+Vetri190591@users.noreply.github.com>
2023-07-20 13:06:35 +0100

    Update fewshot_examples.py

--- api/src/fewshot_examples.py
+++ api/src/fewshot_examples.py
@@ -22,9 +22,14 @@ WITH distinct c, embedding
 WITH c, gds.similarity.cosine(c.embedding, embedding) AS score
 ORDER BY score DESC LIMIT 3
 RETURN c.text, score
-
+#What are the relationships associated with the batch node?
+MATCH (n) MATCH ()-[r]->() RETURN n, r
+#What are the properties of my node?
+Match (n) return properties(n), ID(n)
 When searching for specific information in the text chunks, never use the CONTAINS clause, but always use the apoc.ml.openai.embedding
 and gds.similarity.cosine functions as shown in the examples.
 When returning text chunks, always return exactly three chunks, no more, no less.
 Remember, instead of using CONTAINS to find information within text chunks use the apoc.ml.openai.embedding and gds.similarity.cosine functions.
+"""
+mber, instead of using CONTAINS to find information within text chunks use the apoc.ml.openai.embedding and gds.similarity.cosine functions.
 """
