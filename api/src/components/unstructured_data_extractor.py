ca04662cc1a3e4d6be1e5a9744d299c37e11333a
Noah Mayerhofer <noahmay@users.noreply.github.com>
2023-08-17 14:55:23 +0200

    disable online demo

--- api/src/components/unstructured_data_extractor.py
+++ api/src/components/unstructured_data_extractor.py
@@ -15,7 +15,7 @@ def generate_system_message_with_schema() -> str:
 You are a data scientist working for a company that is building a graph database. Your task is to extract information from data and convert it into a graph database.
 Provide a set of Nodes in the form [ENTITY, TYPE, PROPERTIES] and a set of relationships in the form [ENTITY1, RELATIONSHIP, ENTITY2, PROPERTIES]. 
 Pay attention to the type of the properties, if you can't find data for a property set it to null. Don't make anything up and don't add any extra data. If you can't find any data for a node or relationship don't add it.
-Only add nodes and relationships that are part of the schema.
+Only add nodes and relationships that are part of the schema. If you don't get any relationships in the schema only add nodes.
 
 Example:
 Schema: Nodes: [Person {age: integer, name: string}] Relationships: [Person, roommate, Person]
