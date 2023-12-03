f2f6a533d9764675a7c116b416542983db43dacf
Yunus Barlas <yunusbarlas@gmail.com>
2023-12-03 13:50:12 +0300

    Fix company report problem and separate report and company list endpoints in example env (#73)

--- ui/src/report-generation/utils/fetch-example-companies.ts
+++ ui/src/report-generation/utils/fetch-example-companies.ts
@@ -6,8 +6,11 @@ type JSONResponse = {
 };
 
 export const getCompanies = async () => {
-  const response = await fetch(
-    `${import.meta.env.VITE_REPORT_DATA_ENDPOINT}/companyReport/list`,
+  const LIST_EXAMPLE_COMPANIES_URI =
+      import.meta.env.VITE_REPORT_COMPANY_LIST_ENDPOINT ??
+      "http://localhost:7860/companyReport/list";
+
+  const response = await fetch(LIST_EXAMPLE_COMPANIES_URI,
     {
       method: "POST",
     }
